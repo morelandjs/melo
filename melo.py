93da77e5f6345cc30629d064da1d0f5bae7ae4de
J. Scott Moreland <morelandjs@gmail.com>
2018-06-17 23:10:40 -0400

    working version

--- melo.py
+++ melo.py
@@ -2,11 +2,42 @@
 
 from collections import defaultdict
 from datetime import datetime
+import random
 from scipy.special import erf, erfinv
 
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+from sklearn.decomposition import FastICA
+
+
+today = datetime.today()
+
+class ToyModel:
+    def __init__(self, nteams=32):
+        self.nteams = nteams
+        self.teams = ['team_{}'.format(n) for n in range(nteams)]
+        self.means = [np.random.uniform(10, 50) for n in range(nteams)]
+
+    def games(self, size=10**5):
+
+        times = []
+        labels1 = []
+        labels2 = []
+        values = []
+
+        for n in range(size):
+            i1, i2 = random.sample(range(self.nteams), 2)
+            score1 = np.random.normal(self.means[i1], 5)
+            score2 = np.random.normal(self.means[i2], 5)
+
+            times.append(np.datetime64(today) - np.timedelta64(n, 's'))
+            labels1.append(self.teams[i1])
+            labels2.append(self.teams[i2])
+            values.append(score1 - score2)
+
+        return times, labels1, labels2, values
 
 
 class MarginElo:
@@ -17,7 +48,7 @@ class MarginElo:
     Described in https://arxiv.org/abs/1802.00527
 
     """
-    def __init__(self, times, labels1, labels2, values, nlines=200, k=1e-4):
+    def __init__(self, times, labels1, labels2, values, nlines=10, k=0.05):
         """
         To do, document this.
 
@@ -34,13 +65,14 @@ class MarginElo:
             ), axis=0
         )
 
-        self.nlines = nlines
+        lmin, lmax = np.percentile(self.comparisons.value, [0.01, 99.99])
+        self.lines = np.linspace(lmin, lmax, nlines)
 
         self.k = k
 
-        self.lines, self.ratings = self.rate(
-            self.comparisons, self.nlines
-        )
+        self.rating_hcap = self.naive_prior(self.comparisons.value, self.lines)
+
+        self.ratings = self.rate(self.comparisons, self.lines)
 
     def norm_cdf(self, x, loc=0, scale=1):
         """
@@ -51,71 +83,54 @@ class MarginElo:
 
     def naive_prior(self, values, lines):
         """
-        Initial ratings which respect the naive probability P(value > line),
-        determined from the minimum bias value distribution.
+        Rating difference, dR = P(values - lines > 0), that the observed values
+        cover the specified lines.
 
         """
         prob = np.array(
             [(values > line).sum() / values.size for line in lines],
             dtype=float, ndmin=1
         )
-        return np.sqrt(2)/2*erfinv(2*prob - 1)
+
+        return np.sqrt(2)*erfinv(2*prob - 1)
 
     def query_rating(self, time, label):
         """
         Find the last rating preceeding the specified 'time'.
 
         """
-        ratings = [r for t, r in self.ratings[label] if t < time]
+        return list(r for t, r in self.ratings[label] if t < time).pop()
 
-        return ratings.pop()
-
-    def rate(self, comparisons, nlines):
+    def rate(self, comparisons, lines):
         """
         Apply the margin dependent Elo model to the list of binary comparisons.
 
         """
-        median = np.median(comparisons.value)
-        std = np.std(comparisons.value)
-
-        # standardize the data
-        comparisons.value -= median
-        comparisons.value /= std
-
-        # create standardized comparison lines
-        extremum = np.percentile(np.abs(comparisons.value), 99.9)
-        lines = np.linspace(-extremum, extremum, self.nlines)
-
-        # determine naive prior
-        default_rating = self.naive_prior(comparisons.value, lines)
-        current_rating = defaultdict(lambda: default_rating)
+        current_rating = defaultdict(lambda: np.zeros_like(lines))
         ratings = defaultdict(list)
 
         for (time, label1, label2, value) in comparisons:
 
-            # lookup ratings
+            # lookup team ratings
             rating1 = current_rating[label1]
             rating2 = current_rating[label2]
 
             # prior and posterior outcome probabilities
-            prior = self.norm_cdf(rating1 - rating2[::-1])
-            post = np.heaviside(value - lines, 0.5)
+            prior = self.norm_cdf(rating1 - rating2 + self.rating_hcap)
+            post = np.where(value - lines > 0, 1, 0)
 
             # rating change
             rating_change = self.k * (post - prior)
 
             # save current rating
-            current_rating[label1] = rating1 + rating_change
-            current_rating[label2] = rating2 - rating_change
+            current_rating[label1] += rating_change
+            current_rating[label2] -= rating_change
 
             # record ratings
-            ratings[label1].append((time, rating1 + rating_change))
-            ratings[label2].append((time, rating2 - rating_change))
+            ratings[label1].append((time, current_rating[label1]))
+            ratings[label2].append((time, current_rating[label2]))
 
-        lines *= std
-        lines += median
-
-        return lines, ratings
+        return ratings
 
     def prior_cdf(self, time, label1, label2):
         """
@@ -123,12 +138,10 @@ class MarginElo:
         label1 and label2 at specified 'time'.
 
         """
-        # lookup ratings
         rating1 = self.query_rating(time, label1)
         rating2 = self.query_rating(time, label2)
 
-        # prior and posterior outcome probabilities
-        prior = self.norm_cdf(rating1 - rating2[::-1])
+        prior = np.sort(1 - self.norm_cdf(rating1 - rating2 + self.rating_hcap))
 
         return self.lines, prior
 
@@ -138,30 +151,20 @@ class MarginElo:
 
         """
         lines, prior = self.prior_cdf(time, label1, label2)
-        assert np.all(np.diff(prior[::-1]) > 0)
-        return np.interp(np.random.rand(size), prior[::-1], lines[::-1])
+
+        return np.interp(np.random.rand(size), prior, lines)
+
 
 def main():
+    tm = ToyModel(nteams=32)
+    melo = MarginElo(*tm.games(10**5))
+    minbias = melo.comparisons.value
 
-    size = 10**5
-    today = datetime.today()
-    time = datetime(today.year, today.month, today.day)
-    times = [np.datetime64(time) - np.timedelta64(n, 's') for n in range(size)]
-    labels1 = size*['CLE']
-    labels2 = size*['MIA']
-    values = np.random.normal(1, 2, size=size)
+    pred = melo.samples(np.datetime64(today), 'team_0', 'team_1', size=10**7)
 
-    elo = MarginElo(times, labels1, labels2, values)
+    true = np.random.normal(tm.means[0], 5, 10**7) - np.random.normal(tm.means[1], 5, 10**7)
 
-    today = datetime.today()
-    time = np.datetime64(datetime(today.year, today.month, today.day))
-
-    true = np.random.normal(1, 2, size=10**6)
-    pred = elo.samples(time, 'CLE', 'MIA')
-
-    for samples in (true, pred):
-        plt.hist(samples, histtype='step', bins=100, density=True)
-
+    plt.boxplot([minbias, pred, true], showfliers=False)
     plt.show()
 
 
